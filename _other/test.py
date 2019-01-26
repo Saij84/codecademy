afe42d56ea46486d12beeb4943296e4f327922ba
fangc <saij84@gmail.com>
2019-01-27 10:59:20 +1100

    linear regression

---
+++ _other/test.py
@@ -0,0 +1,17 @@
+#Write your function here
+def reversed_list(lst1, lst2):
+  print(lst2)
+
+  test = [3, 2, 1]
+  test.reverse()
+
+
+  print(test)
+  # if lst1 == lst2.reverse():
+  #   return True
+  # else:
+  #   return False
+
+#Uncomment the lines below when your function is done
+print(reversed_list([1, 2, 3], [3, 2, 1]))
+#print(reversed_list([1, 5, 3], [3, 2, 1]))
