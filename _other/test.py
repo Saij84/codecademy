eae32828fab418083b2ee510abf8e7da89c81dbc
fangc <saij84@gmail.com>
2019-02-02 13:57:11 +1100

    update

--- _other/test.py
+++ _other/test.py
@@ -1,2 +1,19 @@
-combo_meals = {1: ["hamburger", "fries"], 2: ["hamburger", "fries", "soda"], 4: ["veggie burger", "salad", "soda"], 6: ["hot dog", "apple slices", "orange juice"]}
-print(combo_meals.get(3, ["hamburger", "fries"]))
+class Circle:
+  pi = 3.14
+
+  def __init__(self, diameter):
+    print("Creating circle with diameter {d}".format(d=diameter))
+    # Add assignment for self.radius here:
+    self.radius = diameter / 2
+
+  def circumference(self):
+    circumference = 2.0 * self.pi * self.radius
+    return circumference
+
+
+medium_pizza = Circle(12)
+medium_pizza.circumference()
+teaching_table = Circle(36)
+teaching_table.circumference()
+round_room = Circle(11460)
+teaching_table.circumference()
